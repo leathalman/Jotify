f666d5fb7159a3bc1aa9f44de1522934e81e37cf
Harrison Leath <hleath@me.com>
2019-05-08 18:27:55 -0400

    Initial Commit

---
+++ Sticky Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sticky Notes
+//
+//  Created by Harrison Leath on 5/8/19.
+//  Copyright © 2019 Harrison Leath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
