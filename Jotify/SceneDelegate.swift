db40409cb5f8104713862d1f55e4dcb07ec1122f
Harrison Leath <hleath@me.com>
2019-11-25 13:33:21 -0500

    Reimplemented automatic saving on background and updated version number

--- Jotify/SceneDelegate.swift
+++ Jotify/SceneDelegate.swift
@@ -64,13 +64,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         // Save changes in the application's managed object context when the application transitions to the background.
         
+        print("App entered background")
+        
         let privacyController = PrivacySettingsController()
         privacyController.removeBlurView(window: window!)
-        
-        print("Backgroud")
-        
+                
         renumberBadgesOfPendingNotifications()
-//        appDelegate?.saveNoteBeforeExiting()
+        appDelegate?.saveNoteBeforeExiting()
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
     
